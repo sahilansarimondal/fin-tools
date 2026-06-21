cb53e3fd9046688dbfbd364192480f1c71953f01
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2026-06-21 22:53:45 +0530

    added seo improvement and new sidemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://truefinancetools.com',
+  integrations: [
+    sitemap({
+      filter: (page) => !page.match(/\/(403|404|500|503)\/?$/),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://truefinancetools.com',
 });
